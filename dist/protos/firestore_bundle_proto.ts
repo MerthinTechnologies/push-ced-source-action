a7d2e2b411abc20f39502ac7cd48ae99a1ac6bd4
horacionunez <horacio@merthin.com>
2022-12-13 22:48:52 -0500

    Update dependencies, update description

--- dist/protos/firestore_bundle_proto.ts
+++ dist/protos/firestore_bundle_proto.ts
@@ -1,4 +1,3 @@
-(()=>{"use strict";var e={};(()=>{e.r=e=>{if(typeof Symbol!=="undefined"&&Symbol.toStringTag){Object.defineProperty(e,Symbol.toStringTag,{value:"Module"})}Object.defineProperty(e,"__esModule",{value:true})}})();if(typeof e!=="undefined")e.ab=__dirname+"/";var r={};e.r(r);
 /**
  * @license
  * Copyright 2020 Google LLC
@@ -14,4 +13,81 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */module.exports=r})();
+ */
+
+import { StructuredQuery, Timestamp, Document } from './firestore_proto_api';
+
+/** Properties of a BundledQuery. */
+export interface BundledQuery {
+  /** BundledQuery parent */
+  parent?: string | null;
+
+  /** BundledQuery structuredQuery */
+  structuredQuery?: StructuredQuery | null;
+
+  /** BundledQuery limitType */
+  limitType?: LimitType | null;
+}
+
+/** LimitType enum. */
+export type LimitType = 'FIRST' | 'LAST';
+
+/** Properties of a NamedQuery. */
+export interface NamedQuery {
+  /** NamedQuery name */
+  name?: string | null;
+
+  /** NamedQuery bundledQuery */
+  bundledQuery?: BundledQuery | null;
+
+  /** NamedQuery readTime */
+  readTime?: Timestamp | null;
+}
+
+/** Properties of a BundledDocumentMetadata. */
+export interface BundledDocumentMetadata {
+  /** BundledDocumentMetadata name */
+  name?: string | null;
+
+  /** BundledDocumentMetadata readTime */
+  readTime?: Timestamp | null;
+
+  /** BundledDocumentMetadata exists */
+  exists?: boolean | null;
+
+  /** The names of the queries in this bundle that this document matches to. */
+  queries?: string[];
+}
+
+/** Properties of a BundleMetadata. */
+export interface BundleMetadata {
+  /** BundleMetadata id */
+  id?: string | null;
+
+  /** BundleMetadata createTime */
+  createTime?: Timestamp | null;
+
+  /** BundleMetadata version */
+  version?: number | null;
+
+  /** BundleMetadata totalDocuments */
+  totalDocuments?: number | null;
+
+  /** BundleMetadata totalBytes */
+  totalBytes?: number | null;
+}
+
+/** Properties of a BundleElement. */
+export interface BundleElement {
+  /** BundleElement metadata */
+  metadata?: BundleMetadata | null;
+
+  /** BundleElement namedQuery */
+  namedQuery?: NamedQuery | null;
+
+  /** BundleElement documentMetadata */
+  documentMetadata?: BundledDocumentMetadata | null;
+
+  /** BundleElement document */
+  document?: Document | null;
+}
